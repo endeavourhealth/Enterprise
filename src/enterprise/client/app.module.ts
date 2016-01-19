5515159bea21642b05bf287346082c92195e751a
ErgoSoftUK <ergosoftuk@gmail.com>
2016-01-19 13:54:10 +0000

    Extracted sidebar from index.html

--- src/enterprise/client/app.module.ts
+++ src/enterprise/client/app.module.ts
@@ -1,6 +1,8 @@
 /// <reference path="lib/angular/angular.d.ts" />
 angular.module('Enterprise', [
+    'SideBar',
     'Dashboard'
 ]);
 
+angular.module('SideBar',[]);
 angular.module('Dashboard',[]);
---
+++ src/enterprise/client/components/sidebar.directive.ts
@@ -0,0 +1,17 @@
+/// <reference path="../lib/angular/angular.d.ts" />
+module SideBar
+{
+    export class SideBarDirective implements ng.IDirective
+    {
+        public restrict: string = "E";
+        public replace: boolean = true;
+        public templateUrl: string = "modules/sidebar/sidebar.html";
+        //public controller: string = 'SideBarController';
+        //public controllerAs: string = 'sidebar';
+        public scope = {};
+    }
+
+    angular
+        .module('SideBar')
+        .directive("sidebar", [() => new SideBar.SideBarDirective()]);
+}

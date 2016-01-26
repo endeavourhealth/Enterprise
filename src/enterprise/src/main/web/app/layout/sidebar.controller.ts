9bcf7757988de8f7ea657492accb426afd1569d6
ErgoSoftUK <ergosoftuk@gmail.com>
2016-01-26 13:22:09 +0000

    Migrated from anchors (a href) to state references (div ui-sref)

--- src/enterprise/src/main/web/app/layout/sidebar.controller.ts
+++ src/enterprise/src/main/web/app/layout/sidebar.controller.ts
@@ -1,20 +1,20 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 module app.layout {
-    class SidebarController {
-        menuOptions;
+	class SidebarController {
+		menuOptions;
 
-        constructor () {
-            this.menuOptions = [
-                {Caption: "Dashboard", Url: "dashboard", Icon: "glyphicon-dashboard"},
-                {Caption: "Library", Url: "library", Icon: "glyphicon-book"},
-                {Caption: "Reports", Url: "reports", Icon: "glyphicon-file"},
-                {Caption: "Administration", Url: "admin", Icon: "glyphicon-cog"},
-                {Caption: "Audit", Url: "audit", Icon: "glyphicon-check"}
-            ];
-        }
-    }
+		constructor() {
+			this.menuOptions = [
+				{Caption: "Dashboard", State: "dashboard", Icon: "glyphicon-dashboard"},
+				{Caption: "Library", State: "library", Icon: "glyphicon-book"},
+				{Caption: "Reports", State: "reports", Icon: "glyphicon-file"},
+				{Caption: "Administration", State: "admin", Icon: "glyphicon-cog"},
+				{Caption: "Audit", State: "audit", Icon: "glyphicon-check"}
+			];
+		}
+	}
 
-    angular.module('app.layout')
-        .controller('SidebarController', SidebarController);
+	angular.module('app.layout')
+		.controller('SidebarController', SidebarController);
 }

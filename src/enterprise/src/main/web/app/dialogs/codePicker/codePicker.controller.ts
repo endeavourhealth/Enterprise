0e99c0e93c21e13a947862b933bb1ce77504c4fe
ErgoSoftUK <ergosoftuk@gmail.com>
2016-02-05 17:23:14 +0000

    Further code picker work

--- src/enterprise/src/main/web/app/dialogs/codePicker/codePicker.controller.ts
+++ src/enterprise/src/main/web/app/dialogs/codePicker/codePicker.controller.ts
@@ -19,6 +19,10 @@ module app.dialogs {
 			this.selectedItems = this.getSelectedItems();
 		}
 
+		toggle(node : any) {
+			node.show = !node.show;
+		}
+
 		getButtonProperties(item : CodeSearchResult) {
 			if (item.matches.length === 0) {
 				return 'btn-danger disabled';
@@ -96,6 +100,55 @@ module app.dialogs {
 			]
 				;
 		}
+
+		data = [{
+			'id': 1,
+			'title': 'node1',
+			'nodes': [
+				{
+					'id': 11,
+					'title': 'node1.1',
+					'nodes': [
+						{
+							'id': 111,
+							'title': 'node1.1.1',
+							'nodes': []
+						}
+					]
+				},
+				{
+					'id': 12,
+					'title': 'node1.2',
+					'nodes': []
+				}
+			]
+		}, {
+			'id': 2,
+			'title': 'node2',
+			'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
+			'nodes': [
+				{
+					'id': 21,
+					'title': 'node2.1',
+					'nodes': []
+				},
+				{
+					'id': 22,
+					'title': 'node2.2',
+					'nodes': []
+				}
+			]
+		}, {
+			'id': 3,
+			'title': 'node3',
+			'nodes': [
+				{
+					'id': 31,
+					'title': 'node3.1',
+					'nodes': []
+				}
+			]
+		}];
 	}
 
 	angular

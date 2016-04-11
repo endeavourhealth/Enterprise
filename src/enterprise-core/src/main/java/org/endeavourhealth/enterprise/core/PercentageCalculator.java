7d203e9bdbb323753117edba35cf0a5bb52f239c
Drew Littler <drewlittler@outlook.com>
2016-04-11 14:45:30 +0100

    Changes PercentageCalculator

--- src/enterprise-core/src/main/java/org/endeavourhealth/enterprise/core/PercentageCalculator.java
+++ src/enterprise-core/src/main/java/org/endeavourhealth/enterprise/core/PercentageCalculator.java
@@ -8,18 +8,30 @@ public final class PercentageCalculator {
     private static final NumberFormat FORMATTER = new DecimalFormat("##.##");
 
     public static double calculatePercentage(double denominator, double numerator) {
+        if (numerator > denominator) {
+            throw new IllegalArgumentException("Numerator " + numerator + " cannot be larger that denominator " + denominator);
+        }
         return (numerator * 100d) / denominator;
     }
     public static String calculatorPercentString(double denominator, double numerator) {
-        double per = calculatePercentage(denominator, numerator);
-        if (per == 0d) {
+
+        double percentage = calculatePercentage(denominator, numerator);
+
+        if (percentage == 0d) {
             return "0%";
-        } else if (per == 100d) {
+        } else if (percentage == 100d) {
             return "100%";
         } else {
-            return FORMATTER.format(per) + "%";
-        }
 
+            //prevent any rounding to 0 or 100 by spotting these and adjusting the percentage to the next acceptable value
+            if (percentage < 0.01d) {
+                percentage = 0.01d;
+            } else if (percentage > 99.99d) {
+                percentage = 99.99d;
+            }
+
+            return FORMATTER.format(percentage) + "%";
+        }
 
     }
 }

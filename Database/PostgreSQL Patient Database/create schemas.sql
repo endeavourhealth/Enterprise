ab02cfd9beeabfb4bf112320e827c5abc90affeb
Drew Littler <drewlittler@hotmail.com>
2016-09-15 10:15:01 +0100

    Adding PostgreSQL scripts for new Enterprise patient database

---
+++ Database/PostgreSQL Patient Database/create schemas.sql
@@ -0,0 +1,15 @@
+
+-- Schema: admin
+
+-- DROP SCHEMA admin;
+
+CREATE SCHEMA admin
+  AUTHORIZATION postgres;
+
+
+-- Schema: clinical
+
+-- DROP SCHEMA clinical;
+
+CREATE SCHEMA clinical
+  AUTHORIZATION postgres;

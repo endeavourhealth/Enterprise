cd372667046551e86a0a7c4b4ed74a9616c4b8fe
Drew Littler <drewlittler@hotmail.com>
2017-01-10 14:47:59 +0000

    Change DB creation SQL

--- Database/PostgreSQL Patient Database/create database.sql
+++ Database/PostgreSQL Patient Database/create database.sql
@@ -6,6 +6,6 @@ CREATE DATABASE enterprise_data
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'English_United Kingdom.1252'
-       LC_CTYPE = 'English_United Kingdom.1252'
+       LC_COLLATE = 'en_GB.UTF-8'
+       LC_CTYPE = 'en_GB.UTF-8'
        CONNECTION LIMIT = -1;

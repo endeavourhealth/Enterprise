70cf225fc4ddd5faa7416cbed96a5099d73ea6ef
ergosoftuk <qPellHGfNE4FpAnd0D0a>
2017-04-12 11:08:10 +0100

    Update webpack version

--- src/enterprise-web/src/main/frontend/webpack.config.js
+++ src/enterprise-web/src/main/frontend/webpack.config.js
@@ -8,38 +8,47 @@ module.exports = {
 	},
 	output: {
 		filename: './[name].bundle.js',
-		path: '../webapp'
+		path: __dirname + '/../webapp'
 	},
 	resolve: {
-		extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+		extensions: ['.webpack.js', '.web.js', '.ts', '.js']
 	},
 	module: {
 		loaders: [
 			{ test: /\.ts$/, loader: 'awesome-typescript-loader' },
-			{ test: /\.html/, loader: 'raw' },
+			{ test: /\.html/, loader: 'raw-loader' },
 			{ test: /\.css$/, loader: "style-loader!css-loader" },
-			{ test: /\.less$/, loader: "style!css!less" },
-			{	test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url' },
-			{ test:  /\.(jpe?g|png|gif|svg)$/i, loader: 'file' }
+			{ test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+			{	test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' },
+			{ test:  /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' }
 		]
 	},
 	externals: {
 		"jquery": "jQuery"
 	},
 	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: [
+				// Application
+				'app',
+
+				// 3rd Party
+				'vendor'
+			]
+		}),
+		new webpack.ContextReplacementPlugin(
+			/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+			__dirname
+		),
 		new HtmlWebpackPlugin(
 		{
 			template: 'index.ejs',
 			inject: 'body'
 		}),
-		new webpack.optimize.DedupePlugin()
 	],
 	devServer: {
 		inline: true,
 		contentBase: '..\\webapp',
-		watch: true,
-		progress: true,
-		colors: true,
 
 		proxy: {
 			'/api': { target: 'http://localhost:8000' },
